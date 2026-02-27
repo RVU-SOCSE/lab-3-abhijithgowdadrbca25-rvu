94a219e32c5a113933684c3ef4c38d30d2cde6c1
ABHIJITH GOWDA D R <abhijithgowdadrbca25@rvu.edu.in>
2026-02-27 12:02:38 +0530

    Add files via upload

---
+++ prog1.sh
@@ -0,0 +1,22 @@
+echo "job 1:Display Date"
+date 
+sleep 3
+
+echo "job 2: Dispplay current users"
+whoami
+sleep 3
+
+echo "job 3: Display Calender"
+cal
+sleep 3
+
+echo "job 4: List in files"
+ls
+sleep 3
+
+echo "job 5: Display my system date"
+date +%T 
+sleep 3
+
+echo "job 6"
+pwd
